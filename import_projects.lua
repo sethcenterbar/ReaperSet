d12df62fcaf0ba173bfe0d9ccf3dcba241896a78
Seth Centerbar <sethcenterbar@gmail.com>
2024-08-24 00:19:42 -0400

    feat: copy tempo map from each song

--- import_projects.lua
+++ import_projects.lua
@@ -13,6 +13,26 @@ project_files = {
     dir .. "song3/song3.RPP",
 }
 
+
+    -- Copy tempo map from the original project
+function getTempoMap(project)
+    numTempos = reaper.CountTempoTimeSigMarkers(project)
+    tempoMarkers = {}
+    for i = 0, numTempos - 1 do
+        local retval, timepos, measurepos, beatpos, bpm, timesig_num, timesig_denom, lineartempo = reaper.GetTempoTimeSigMarker(project, i)
+        table.insert(tempoMarkers, {timepos, bpm, timesig_num, timesig_denom, lineartempo})
+    end
+    return tempoMarkers
+end
+
+function setTempoMap(current_position, tempoMarkers, targetProject)
+    -- Paste the copied tempo map
+
+    for _, marker in ipairs(tempoMarkers) do
+        reaper.SetTempoTimeSigMarker(targetProject, -1, marker[1] + current_position, -1, -1, marker[2], marker[3], marker[4], marker[5], false)
+    end
+end
+
 local date_string = os.date("%Y-%m-%d_%H-%M-%S")
 
 local setlist = dir .. "setlists/" .. date_string .. "setlist.rpp"
@@ -33,11 +53,14 @@ for index, project in ipairs(project_files) do
     reaper.Main_OnCommand(40035, 0) -- Select all items in the current project
     reaper.Main_OnCommand(40698, 0) -- Copy all selected items
 
+    tempoMap = getTempoMap(project)
+
     reaper.Main_openProject("noprompt:" .. setlist)
 
     local region_start = reaper.GetCursorPositionEx(setlist)
 
     reaper.Main_OnCommand(40043, 0)  -- Go to the end of the project
+    setTempoMap(region_start, tempoMap, setlist)
     reaper.Main_OnCommand(41748, 0)  -- Paste items
     
     local region_end = reaper.GetCursorPositionEx(setlist)
